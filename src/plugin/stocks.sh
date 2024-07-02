10bf071daae60fd88eade56f17d682129c9047c7
Alexander Figueiras <figueiras.alexander@gmail.com>
2024-07-02 15:46:56 -0400

    Testing for text change

--- src/plugin/stocks.sh
+++ src/plugin/stocks.sh
@@ -6,16 +6,19 @@ plugin_stocks_icon=$(get_tmux_option "@theme_plugin_stocks_icon" "󰝚 ")
 plugin_stocks_accent_color=$(get_tmux_option "@theme_plugin_stocks_accent_color" "blue7")
 plugin_stocks_accent_color_icon=$(get_tmux_option "@theme_plugin_stocks_accent_color_icon" "blue0")
 
-plugin_stocks_format_string=$(get_tmux_option "@theme_plugin_stocks_format_string" "")
+plugin_stocks_format_string=$(get_tmux_option "@theme_plugin_stocks_format_string" "HELLO %h")
 
-export plugin_stocks_icon plugin_stocks_accent_color plugin_stocks_accent_color_icon
 
 function load_plugin() {
+    echo "Test"
+
     if ! command -v stocks &>/dev/null; then
 		exit 1
 	fi
 
 	echo "HELLO!"
 }
+
+export plugin_stocks_icon plugin_stocks_accent_color plugin_stocks_accent_color_icon
 
 load_plugin
