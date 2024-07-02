23a13f8b904e141daaa99841d7a62fe6d88fafcd
Alexander Figueiras <figueiras.alexander@gmail.com>
2024-07-02 16:35:32 -0400

    Testing more changes

--- src/plugin/stocks.sh
+++ src/plugin/stocks.sh
@@ -3,14 +3,14 @@ ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 . "$ROOT_DIR/../utils.sh"
 
 # shellcheck disable=SC2005
-plugin_stocks_icon=$(get_tmux_option "@theme_plugin_stocks_icon" "󰝚 ")
+plugin_stocks_icon=$(get_tmux_option "@theme_plugin_stocks_icon" " ")
 plugin_stocks_accent_color=$(get_tmux_option "@theme_plugin_stocks_accent_color" "blue7")
 plugin_stocks_accent_color_icon=$(get_tmux_option "@theme_plugin_stocks_accent_color_icon" "blue0")
 
-plugin_stocks_format_string=$(get_tmux_option "@theme_plugin_stocks_format_string" "HELLO %h")
+plugin_stocks_format=$(get_tmux_option "@theme_plugin_stocks_format" "%D %H:%M:%S")
 
 function load_plugin() {
-    echo "Test"
+    echo "${plugin_stocks_format}"
 }
 
 export plugin_stocks_icon plugin_stocks_accent_color plugin_stocks_accent_color_icon
