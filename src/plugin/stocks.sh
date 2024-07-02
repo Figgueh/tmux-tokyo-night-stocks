d5fab8aca2d96b128766395590a3f6b968144ca0
Alexander Figueiras <figueiras.alexander@gmail.com>
2024-07-02 14:49:28 -0400

    Added more template code

--- src/plugin/stocks.sh
+++ src/plugin/stocks.sh
@@ -2,8 +2,20 @@
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 . "$ROOT_DIR/../utils.sh"
 
+plugin_stocks_icon=$(get_tmux_option "@theme_plugin_stocks_icon" "󰝚 ")
+plugin_stocks_accent_color=$(get_tmux_option "@theme_plugin_stocks_accent_color" "blue7")
+plugin_stocks_accent_color_icon=$(get_tmux_option "@theme_plugin_stocks_accent_color_icon" "blue0")
+
+plugin_playerctl_format_string=$(get_tmux_option "@theme_plugin_stocks_format_string" "")
+
+export plugin_stocks_icon plugin_stocks_accent_color plugin_stocks_accent_color_icon
 
 function load_plugin() {
+    if ! command -v stocks &>/dev/null; then
+		exit 1
+	fi
+
 	echo "HELLO!"
 }
+
 load_plugin
