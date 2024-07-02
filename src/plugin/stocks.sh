5d3bab63e6c24c0be7ee8617dfb3006005f0f40b
Alexander Figueiras <figueiras.alexander@gmail.com>
2024-07-02 19:49:56 -0400

    Changed output to be static

--- src/plugin/stocks.sh
+++ src/plugin/stocks.sh
@@ -10,7 +10,7 @@ plugin_stocks_accent_color_icon=$(get_tmux_option "@theme_plugin_stocks_accent_c
 plugin_stocks_format=$(get_tmux_option "@theme_plugin_stocks_format" "%D %H:%M:%S")
 
 function load_plugin() {
-    echo "${plugin_stocks_format}"
+    echo "HELLLLOOOOOOO!"
 }
 
 export plugin_stocks_icon plugin_stocks_accent_color plugin_stocks_accent_color_icon
