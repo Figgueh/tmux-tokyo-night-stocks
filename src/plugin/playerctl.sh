322b5c18be0f2f6939e680f8bad308c3a5398cb9
Alexander Figueiras <figueiras.alexander@gmail.com>
2024-07-02 20:25:54 -0400

    More changes to see if I can hook

--- src/plugin/playerctl.sh
+++ src/plugin/playerctl.sh
@@ -21,7 +21,7 @@ function load_plugin() {
 		playerctl=$(playerctl metadata --format "$plugin_playerctl_format_string")
 		echo "${playerctl}"
 	else
-		echo "Not Playing"
+		echo "Not Playing - HELLLO"
 	fi
 }
 
